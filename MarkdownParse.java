c3ff3459f4d908fda7e73f6ba5401f97f9d9fa06
Justin Sin <97692918+jusinucsd26@users.noreply.github.com>
2022-01-27 11:08:28 -0800

    Fix test-file4

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -18,13 +18,15 @@ public class MarkdownParse {
             }
             int openParen = markdown.indexOf("(", nextCloseBracket);
             int closeParen = markdown.indexOf(")", openParen);
-            if (markdown.charAt(nextOpenBracket -1) == '!') {
+            if (openParen == -1 || closeParen == -1) {
+                break;
+            }
+            if (nextOpenBracket - 1 != -1 && markdown.charAt(nextOpenBracket - 1) == '!') {
                 currentIndex = closeParen + 1;
                 continue;
             }
             toReturn.add(markdown.substring(openParen + 1, closeParen));
             currentIndex = closeParen + 1;
-            System.out.println(currentIndex);
         }
         return toReturn;
     }
